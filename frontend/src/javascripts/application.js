7afc7ebcaed120fc3ae21a86fd81597fa8cb0669
kaiba <kaibadash@gmail.com>
2016-01-23 22:11:53 +0900

    Request restaurant data and show them

--- frontend/src/javascripts/application.js
+++ frontend/src/javascripts/application.js
@@ -1,21 +1,72 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
 
 console.log("start!");
 
-// tutorial1.js
-var CommentBox = React.createClass({
+var Restaurant = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
+        <div className="restaurant">
+          <h2 className="restaurantAuthor">
+            {this.props.name}
+          </h2>
+          {this.props.children}
+        </div>
     );
   }
 });
-console.log("start render")
+
+var RestaurantBox = React.createClass({
+  requestRestaurants: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        console.log("request success:", data)
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.requestRestaurants();
+  },
+  render: function() {
+    return (
+        <div className="restaurantBox">
+          <h1>Restaurants</h1>
+          <RestaurantList data={this.state.data} />
+        </div>
+    );
+  }
+});
+
+var RestaurantList = React.createClass({
+  render: function() {
+    var restaurantNodes = this.props.data.map(function(restaurant) {
+      return (
+          <Restaurant name={restaurant.name} key={restaurant.id}>
+            {restaurant.name}
+          </Restaurant>
+      );
+    });
+    return (
+        <div className="restaurantList">
+          {restaurantNodes}
+        </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+    <RestaurantBox url="/restaurants.json" />,
+    document.getElementById('content')
 );
 
