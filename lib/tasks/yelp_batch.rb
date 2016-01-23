5b7e46b1369d1bc76d166ac9a4319157ec23d975
yuyataki <takiyour@gmail.com>
2016-01-23 23:14:35 +0900

    issue_9 YelpApiまわり

--- lib/tasks/yelp_batch.rb
+++ lib/tasks/yelp_batch.rb
@@ -9,23 +9,26 @@ module Tasks
         restaurants.each do |rest|
           params = { term: rest.name, limit: 1, category_filter: "food", redius_filter: 200 }
           yelp_result = Yelp.client.search(LOCATION, params).businesses.first
-          if yelp_result.present?
-            rest.update_attributes(
-              postal_code: get_postal_code(yelp_result),
-              city: yelp_result.location.city,
-              addr: yelp_result.location.address.reduce { |str, addr| str += addr },
-              tel: yelp_result.display_phone,
-              lat: yelp_result.location.coordinate.latitude,
-              lon: yelp_result.location.coordinate.longitude,
-              point: yelp_result.rating,
-              url: yelp_result.url,
-              image_url: yelp_result.image_url
-            )
-          end
+          next yelp_result.present?
+          update_restaurant(rest, yelp_result)
         end
       end
 
       private
+
+      def update_restaurant(rest, yelp_result)
+        rest.update_attributes(
+          postal_code: get_postal_code(yelp_result),
+          city: yelp_result.location.city,
+          addr: yelp_result.location.address.reduce { |str, addr| str += addr },
+          tel: yelp_result.display_phone,
+          lat: yelp_result.location.coordinate.latitude,
+          lon: yelp_result.location.coordinate.longitude,
+          point: yelp_result.rating,
+          url: yelp_result.url,
+          image_url: yelp_result.image_url
+        )
+      end
 
       def get_postal_code(yelp_result)
         yelp_result.location.display_address.find { |addr| addr =~ /〒\d{3}-\d{4}/ }
