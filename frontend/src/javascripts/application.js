4f5123d9a71f8cc12065eef3a223d048c6c4cf8a
kaiba <kaibadash@gmail.com>
2016-01-23 21:34:24 +0900

    React start

--- frontend/src/javascripts/application.js
+++ frontend/src/javascripts/application.js
@@ -1,2 +1,21 @@
-// ES 6
-function inc(x, y = 1) { return x += y; }
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+console.log("start!");
+
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+console.log("start render")
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
+
