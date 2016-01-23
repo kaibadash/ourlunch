6c1d3941159438f4f289f47da48ed34088859ddd
yuyataki <takiyour@gmail.com>
2016-01-23 22:59:41 +0900

    issue_9 YelpApiまわり

--- lib/tasks/yelp_batch.rb
+++ lib/tasks/yelp_batch.rb
@@ -3,31 +3,34 @@ require 'yelp'
 module Tasks
   class YelpBatch
     LOCATION = "渋谷"
-    def self.get_all_restaurants
-      restaurants = Restaurant.all
-      restaurants.each do |rest|
-        params = { term: rest.name, limit: 1, category_filter: "food", redius_filter: 200 }
-        yelp_result = Yelp.client.search(LOCATION, params).businesses.first
-        rest.update_attributes(
-          name: yelp_result.name,
-          postal_code: get_postal_code(yelp_result),
-          city: yelp_result.location.city,
-          addr: yelp_result.location.address.reduce { |str, addr| str += addr }
-          tel: yelp_result.display_phone,
-          lat: yelp_result.location.coordinate.latitude,
-          lon: yelp_result.location.coordinate.longitude,
-          point: yelp_result.rate,
-          url: yelp_result.url,
-          image_url: yelp_result.image_url
-        )
+    class << self
+      def get_all_restaurants
+        restaurants = Restaurant.all
+        restaurants.each do |rest|
+          params = { term: rest.name, limit: 1, category_filter: "food", redius_filter: 200 }
+          yelp_result = Yelp.client.search(LOCATION, params).businesses.first
+          if yelp_result.present?
+            rest.update_attributes(
+              postal_code: get_postal_code(yelp_result),
+              city: yelp_result.location.city,
+              addr: yelp_result.location.address.reduce { |str, addr| str += addr },
+              tel: yelp_result.display_phone,
+              lat: yelp_result.location.coordinate.latitude,
+              lon: yelp_result.location.coordinate.longitude,
+              point: yelp_result.rating,
+              url: yelp_result.url,
+              image_url: yelp_result.image_url
+            )
+          end
+        end
       end
-    end
 
-    private
+      private
 
-    def get_postal_code(yelp_result)
-      yelp_result.location.display_address.find { |addr| addr =~ /〒\d{3}-\d{4}/ }
-      $&
+      def get_postal_code(yelp_result)
+        yelp_result.location.display_address.find { |addr| addr =~ /〒\d{3}-\d{4}/ }
+        $&
+      end
     end
   end
 end
