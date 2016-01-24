951db9c9b81886559657dc3e52fa1a7318e674d5
muramurasan <kiyoshi.kodama@gmail.com>
2016-01-24 10:34:39 +0900

    extract api access method.

--- lib/tasks/yelp_batch.rb
+++ lib/tasks/yelp_batch.rb
@@ -5,13 +5,22 @@ module Tasks
     LOCATION = "渋谷"
     INTERVAL_YELP_ACCESS_TIME = 3
     class << self
+      def create_restaurants
+        File.open('data/restaurant_name.txt', 'r:utf-8') do |f|
+          f.each_line do |line|
+            # TODO:ifの条件問い合わせをmodelのscopeに移動
+            p if Restaurant.where(name: line.chomp).blank?
+            #puts line if Restaurant.where(name: line.chomp).presents?
+          end
+        end
+      end
+
       def get_all_restaurants
         restaurants = Restaurant.all
         restaurants.each do |rest|
-          params = { term: rest.name, limit: 1, category_filter: "food", redius_filter: 200 }
-          yelp_result = Yelp.client.search(LOCATION, params).businesses.first
-          next yelp_result.present?
-          update_restaurant(rest, yelp_result)
+          yelp = yelp_result(rest)
+          next yelp.present?
+          update_restaurant(rest, yelp)
           sleep(INTERVAL_YELP_ACCESS_TIME) # yelpのAPIを繰り返し叩くのにインターバルを持たせる.
         end
       end
@@ -35,6 +44,11 @@ module Tasks
       def get_postal_code(yelp_result)
         yelp_result.location.display_address.find { |addr| addr =~ /〒\d{3}-\d{4}/ }
         $&
+      end
+
+      def yelp_result(rest)
+        params = { term: rest.name, limit: 1, category_filter: "food", redius_filter: 200 }
+        Yelp.client.search(LOCATION, params).businesses.first
       end
     end
   end
